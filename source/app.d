6cd6e07f921da320adcb813eee8835700c171556
James Punnett <jppunnett@yahoo.com>
2020-11-21 14:48:58 -0400

    Test basic load, write, rotate, save.

--- source/app.d
+++ source/app.d
@@ -1,6 +1,11 @@
 import std.stdio;
+import dlib.image;
 
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+	auto img = loadImage("test.png");
+	drawText(img, "Help!", 0, 0, color3(0xFFFFFF));
+	saveImage(img, "help.png");
+	auto rotated = rotateImage(img, 90);
+	saveImage(rotated, "rotated.png");
 }
